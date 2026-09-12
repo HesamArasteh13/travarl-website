d5654bb55dab5d99240acb395b7c15fb3cfa7887
HesamArasteh13 <hesam1382arasthe@gmail.com>
2026-09-12 06:12:13 +0330

    about.html add jinja

--- home_page/urls.py
+++ home_page/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'home_page'
 
 urlpatterns = [
-    path('', index , name='index')
+    path('', index , name='index'),
+    path('about/', about , name='about'),
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index (request):
     return render (request , 'home_page/index.html')
+
+def about(request):
+    return render (request , 'home_page/about.html')
