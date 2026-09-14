8011d67fab6fc4a9c2e436aa6fb39540f6124987
HesamArasteh13 <hesam1382arasthe@gmail.com>
2026-09-14 19:19:26 +0330

    add ContactAdmin on admin on home_page

--- home_page/admin.py
+++ home_page/admin.py
@@ -3,4 +3,10 @@ from.models import *
 
 # Register your models here.
 
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name','email','created_date')
+    list_filter = ('email',)
+    search_fields = ('name','message')
+
+admin.site.register(Contact , ContactAdmin)
