8db36a0a231205ba53c767dc7db9373ed09b75d8
HesamArasteh13 <hesam1382arasthe@gmail.com>
2026-09-18 00:50:53 +0330

    add postcategories inclusion tag and use it in bloghome.html

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 
 register = template.Library()
 
@@ -21,3 +22,12 @@ def snipets (value , arg=50):
 def latestposts ():
     posts = Post.objects.filter(status = 1).order_by('published_date')
     return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def post_categories ():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories :
+        cat_dict[name] =  posts.filter(category=name).count()
+    return {'categories':cat_dict}
