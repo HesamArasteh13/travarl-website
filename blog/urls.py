9a9571ed5784d69fd96b43b08403d8b8d41f512b
HesamArasteh13 <hesam1382arasthe@gmail.com>
2026-09-18 01:28:08 +0330

    change category left side and I DON'T KNOW WHAT HAS HAPPEND!

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path ('' , index , name='index'),
     path ('<slug:test>' , single , name='single'),
+    path ('category/<str:cat_name>' , blog_category , name='category'),
     path ('test/' , test , name='test'),
 ]
