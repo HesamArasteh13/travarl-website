f8e2081bedaf85a0706f995ec2fe8df0afc8ca11
HesamArasteh13 <hesam1382arasthe@gmail.com>
2026-09-17 23:30:15 +0330

    add media in setting and use inclosion tag in single , blog home.html and change pictures in admin panel

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -17,7 +17,7 @@ def function ():
 def snipets (value , arg=50):
     return value[:arg]+ ' ...'
 
-@register.inclusion_tag('blog/popularposts.html')
-def popularposts ():
+@register.inclusion_tag('blog/blog-latest.posts.html')
+def latestposts ():
     posts = Post.objects.filter(status = 1).order_by('published_date')
     return {'posts':posts}
