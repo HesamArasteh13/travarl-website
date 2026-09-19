b97d0efb2d00b90fe6c2a95703b929839fabfde8
HesamArasteh13 <hesam1382arasthe@gmail.com>
2026-09-19 11:36:57 +0330

    save some data in database

--- home_page/views.py
+++ home_page/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from home_page.models import Contact
 
 def index (request):
     return render (request , 'home_page/index.html')
@@ -13,5 +12,14 @@ def contact (request):
 
 def test (request):
     if request.method == 'POST':
-        print (request.POST.get('name'))
+         name = request.POST.get('name')
+         email = request.POST.get('email')
+         message = request.POST.get('message')
+         subject = request.POST.get('subject')
+         c = Contact()
+         c.name = name
+         c.email = email
+         c.message = message 
+         c.subject = subject
+         c.save()
     return render (request , 'test.html')
